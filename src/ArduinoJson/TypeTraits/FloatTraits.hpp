e3459d0e8584b38b06cf5f02ea55cac6dc60fe37
Benoit Blanchon <github@benoitblanchon.fr>
2017-07-07 23:02:25 +0200

    replaced constant in makeFloat

--- src/ArduinoJson/TypeTraits/FloatTraits.hpp
+++ src/ArduinoJson/TypeTraits/FloatTraits.hpp
@@ -32,16 +32,25 @@ struct FloatTraits<T, 8 /*64bits*/> {
   template <typename TExponent>
   static T make_float(T m, TExponent e) {
     if (e > 0) {
-      static T factors[] = {1e1, 1e2, 1e4, 1e8, 1e16, 1e32, 1e64, 1e128, 1e256};
-      for (uint8_t index = 0; index < 9; e >>= 1, index++) {
+      static T factors[] = {
+          1e1, 1e2, 1e4, 1e8, 1e16, 1e32,
+          // workaround to support platforms with single precision literals
+          forge(0x4D384F03, 0xE93FF9F5), forge(0x5A827748, 0xF9301D32),
+          forge(0x75154FDD, 0x7F73BF3C)};
+      for (uint8_t index = 0; e != 0; index++) {
         if (e & 1) m *= factors[index];
+        e >>= 1;
       }
     } else {
       e = TExponent(-e);
-      static T factors[] = {1e-1,  1e-2,  1e-4,   1e-8,  1e-16,
-                            1e-32, 1e-64, 1e-128, 1e-256};
-      for (uint8_t index = 0; index < 9; e >>= 1, index++) {
+      static T factors[] = {
+          1e-1, 1e-2, 1e-4, 1e-8, 1e-16, 1e-32,
+          // workaround to support platforms with single precision literals
+          forge(0x32A50FFD, 0x44F4A73D), forge(0x255BBA08, 0xCF8C979D),
+          forge(0x0AC80628, 0x64AC6F43)};
+      for (uint8_t index = 0; e != 0; index++) {
         if (e & 1) m *= factors[index];
+        e >>= 1;
       }
     }
     return m;
@@ -80,14 +89,16 @@ struct FloatTraits<T, 4 /*32bits*/> {
   static T make_float(T m, TExponent e) {
     if (e > 0) {
       static T factors[] = {1e1f, 1e2f, 1e4f, 1e8f, 1e16f, 1e32f};
-      for (uint8_t index = 0; e != 0; e >>= 1, index++) {
+      for (uint8_t index = 0; e != 0; index++) {
         if (e & 1) m *= factors[index];
+        e >>= 1;
       }
     } else {
       e = -e;
       static T factors[] = {1e-1f, 1e-2f, 1e-4f, 1e-8f, 1e-16f, 1e-32f};
-      for (uint8_t index = 0; e != 0; e >>= 1, index++) {
+      for (uint8_t index = 0; e != 0; index++) {
         if (e & 1) m *= factors[index];
+        e >>= 1;
       }
     }
     return m;
