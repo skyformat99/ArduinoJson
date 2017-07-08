37fc6ca29a169768371d1e315f448121c06596ec
Benoit Blanchon <github@benoitblanchon.fr>
2017-07-08 21:36:10 +0200

    Reuse

--- src/ArduinoJson/Polyfills/normalize.hpp
+++ src/ArduinoJson/Polyfills/normalize.hpp
@@ -17,6 +17,8 @@ int16_t normalize(T& value) {
   using namespace TypeTraits;
   int16_t powersOf10 = 0;
 
+  // TODO: remove hardcoded 8
+
   if (value >= ARDUINOJSON_POSITIVE_EXPONENTIATION_THRESHOLD) {
     int bit = 256;
     for (int8_t index = 8; index >= 0; index--) {
@@ -29,43 +31,13 @@ int16_t normalize(T& value) {
   }
 
   if (value > 0 && value <= ARDUINOJSON_NEGATIVE_EXPONENTIATION_THRESHOLD) {
-#if ARDUINOJSON_DOUBLE_IS_64BITS
-    if (value / 10 < 1e-256) {
-      value *= 1e256;
-      powersOf10 = int16_t(powersOf10 - 256);
-    }
-    if (value / 10 < 1e-128) {
-      value *= 1e128;
-      powersOf10 = int16_t(powersOf10 - 128);
-    }
-    if (value / 10 < 1e-64) {
-      value *= 1e64;
-      powersOf10 = int16_t(powersOf10 - 64);
-    }
-#endif
-    if (value / 10 < 1e-32) {
-      value *= 1e32;
-      powersOf10 = int16_t(powersOf10 - 32);
-    }
-    if (value / 10 < 1e-16) {
-      value *= 1e16;
-      powersOf10 = int16_t(powersOf10 - 16);
-    }
-    if (value / 10 < 1e-8) {
-      value *= 1e8;
-      powersOf10 = int16_t(powersOf10 - 8);
-    }
-    if (value / 10 < 1e-4) {
-      value *= 1e4;
-      powersOf10 = int16_t(powersOf10 - 4);
-    }
-    if (value / 10 < 1e-2) {
-      value *= 1e2;
-      powersOf10 = int16_t(powersOf10 - 2);
-    }
-    if (value / 10 < 1e-1) {
-      value *= 1e1;
-      powersOf10 = int16_t(powersOf10 - 1);
+    int bit = 256;
+    for (int8_t index = 8; index >= 0; index--) {
+      if (value / 10 < FloatTraits<T>::negativeBinaryPowerOfTen(index)) {
+        value *= FloatTraits<T>::positiveBinaryPowerOfTen(index);
+        powersOf10 = int16_t(powersOf10 - bit);
+      }
+      bit >>= 1;
     }
   }
 
